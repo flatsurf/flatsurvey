7f1387b37ea35efaff4bc28eb868f859d92f1d1c
Vincent Delecroix <vincent.delecroix@u-bordeaux.fr>
2020-04-07 16:17:06 +0200

    update triangle script

--- survey/triangles/triangle_survey.py
+++ survey/triangles/triangle_survey.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+#
+# Typical usage
+#
+#    $ sage -python triangle_survey.py 2 4 7 --bound 3 --test-complete-periodicity True
+#    $ sage -python triangle_survey.py 2 4 11 --bound 4 --test-complete-periodicity False
+#
 ######################################################################
 #  This file is part of flatsurf.
 #
@@ -21,22 +26,28 @@
 #  along with flatsurf. If not, see <https://www.gnu.org/licenses/>.
 ######################################################################
 
-# TODO: Turn parts of this into a benchmark
-
 import argparse
 import sys
 
-import cppyy
 from sage.all import gcd, QQ
 from sage.arith.all import gcd
-from pyflatsurf import flatsurf, Surface, GL2ROrbitClosure
-import flatsurf as sage_flatsurf
-from pyeantic.sage_conversion import sage_nf_to_eantic, sage_nf_elem_to_eantic
-import surface_dynamics
+import flatsurf
+
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
 
 parser = argparse.ArgumentParser(description='Survey a Triangle')
 parser.add_argument('angles', metavar='N', type=int, nargs='+')
-parser.add_argument('--bound', type=int, default=10)
+parser.add_argument('--bound', type=int, default=10, help="bound for the length of saddle connections")
+parser.add_argument('--depth', type=int, default=512, help="depth for the flow decomposition")
+parser.add_argument('--test-complete-periodicity', type=str2bool, default=True, help="whether to look for complete periodicity")
 
 args = parser.parse_args()
 
@@ -48,44 +59,19 @@ if gcd([a, b, c]) != 1:
 print("Unfolding %s"%(args.angles))
 
 # Unfold the (a, b, c) triangle with sage-flatsurf
-T = sage_flatsurf.polygons.triangle(*args.angles)
-S = sage_flatsurf.similarity_surfaces.billiard(T)
+E = flatsurf.EquiangularPolygons(*args.angles)
+T = E([1])
+S = flatsurf.similarity_surfaces.billiard(T, rational=True)
 S = S.minimal_cover(cover_type="translation")
 
 # Dynamical orbit closure computation
-O = GL2ROrbitClosure(S)
-print(O.surface) # underlying pyflatsurf surface
+O = flatsurf.GL2ROrbitClosure(S)
+print(O._surface) # underlying pyflatsurf surface
 
-# prediction for the orbit closure
-# (this is an upper bound)
-N = n = a + b + c
-a = QQ((a,n))
-b = QQ((b,n))
-c = QQ((c,n))
-if N%2:
-    N *= 2
-
-quad = False # whether we have a quadratic differential
-angles = {} # angle in the cover (angle, mult)
-for x in [a,b,c]:
-    d = x.denominator()
-    y = x.numerator()
-    if d%2:
-        y *= 2
-        d *= 2
-    quad = quad or (y % 2 == 1)
-    mult = N // d
-    if y in angles:
-        angles[y] += N//d
-    else:
-        angles[y] = N//d
-
-if quad:
-    quad = True
-    ambient_locus = surface_dynamics.QuadraticStratum({y-2: mult for y,mult in angles.items()})
-else:
-    quad = False
-    ambient_locus = surface_dynamics.AbelianStratum({(y-2)//2: mult for y,mult in angles.items()})
+# Prediction for orbit closure
+ambient_locus = E.billiard_unfolding_stratum("half-translation")
+ambient_dim = ambient_locus.dimension()
+quad = (ambient_locus != E.billiard_unfolding_stratum("translation"))
 
 sc_completely_periodic = True
 cyl_completely_periodic = True
@@ -94,8 +80,8 @@ undetermined = 0
 explored = 0
 
 # computation
-dim = O.U.dimension()
-for d in O.decompositions_depth_first(args.bound, 1024):
+dim = O.dimension()
+for d in O.decompositions_depth_first(args.bound, args.depth):
     print("Investigating in direction %s "%d.u, end='')
     sys.stdout.flush()
     explored += 1
@@ -106,25 +92,26 @@ for d in O.decompositions_depth_first(args.bound, 1024):
         undetermined += 1
         continue
 
-    if sc_completely_periodic or cyl_completely_periodic or parabolic:
-        cyl_completely_periodic = cyl_completely_periodic and (ncyl == 0 or nmin == 0)
-        sc_completely_periodic = sc_completely_periodic and (nmin == 0)
+    if args.test_complete_periodicity:
+        if sc_completely_periodic or cyl_completely_periodic or parabolic:
+            cyl_completely_periodic = cyl_completely_periodic and (ncyl == 0 or nmin == 0)
+            sc_completely_periodic = sc_completely_periodic and (nmin == 0)
 
-        if ncyl and parabolic:
-            parabolic = parabolic and d.parabolic()
+            if ncyl and parabolic:
+                parabolic = parabolic and d.parabolic()
 
     O.update_tangent_space_from_flow_decomposition(d)
-    new_dim = O.U.dimension()
+    new_dim = O.dimension()
     if new_dim != dim:
         dim = new_dim
-        print("new tangent vector: dim=%d"%dim)
-    if dim == ambient_locus.dimension() and not cyl_completely_periodic:
+        print("new tangent vector: dim=%d/%d" % (dim, ambient_dim))
+    if dim == ambient_locus.dimension() and (not args.test_complete_periodicity or not cyl_completely_periodic):
         print("nothing more to be investigated")
         break
 else:
     print("up to bound %s on the length of saddle connection"%args.bound)
 
-closure_dim = O.U.dimension()
+closure_dim = O.dimension()
 absolute_dim = O.absolute_dimension()
 print("Unfolding %s"%(args.angles))
 print("%d directions explored (%d undetermined)"%(explored, undetermined))
@@ -134,15 +121,21 @@ else:
     print("ambient locus: %s (of dimension %s)"%(ambient_locus, ambient_locus.dimension()))
 print("orbit closure dimension: %d"%closure_dim)
 print("rank: %s"%QQ((absolute_dim,2)))
-if sc_completely_periodic and undetermined:
-    print("saddle connection completely periodic: ? (probably false)")
+
+if args.test_complete_periodicity:
+    if sc_completely_periodic and undetermined:
+        print("saddle connection completely periodic: ? (probably false)")
+    else:
+        print("saddle connection completely periodic: %s"%sc_completely_periodic)
+    if cyl_completely_periodic and undetermined:
+        print("cylinder completely periodic: ? (probably true)")
+    else:
+        print("cylinder completely periodic: %s"%cyl_completely_periodic)
+    if parabolic and undetermined:
+        print("parabolic: ? (probably true)")
+    else:
+        print("parabolic: %s"%parabolic)
 else:
-    print("saddle connection completely periodic: %s"%sc_completely_periodic)
-if cyl_completely_periodic and undetermined:
-    print("cylinder completely periodic: ? (probably true)")
-else:
-    print("cylinder completely periodic: %s"%cyl_completely_periodic)
-if parabolic and undetermined:
-    print("parabolic: ? (probably true)")
-else:
-    print("parabolic: %s"%parabolic)
+    print("saddle connection completely periodic: untested")
+    print("cylinder completely periodic: untested")
+    print("parabolic: untested")
