3d58a66c93a0504e8df10ed38b04dc22663cbf48
Julian Rüth <julian.rueth@fsfe.org>
2021-06-25 13:43:12 +0200

    make flatsurvey pip-installable

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+from distutils.core import setup
+
+setup(
+    name = 'flatsurvey',
+    version = '0.1.0',
+    packages = ['flatsurvey',],
+    license = 'GPL 3.0+',
+    long_description = open('README.md').read(),
+    include_package_data=True,
+    entry_points = {
+        'console_scripts': [
+            'flatsurvey=flatsurvey.__main__:survey',
+            'flatsurvey-worker=flatsurvey.worker.__main__:worker',
+        ],
+    },
+)
+
