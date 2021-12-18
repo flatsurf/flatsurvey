6d1f0b3f5b6973553025496ba0519685a037bd37
Julian Rüth <julian.rueth@fsfe.org>
2021-12-17 23:57:11 -0600

    Fix installation of flatsurvey

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name = 'flatsurvey',
     version = '0.1.0',
-    packages = ['flatsurvey', 'flatsurvey.cache', 'flatsurvey.jobs', 'flatsurvey.pipeline', 'flatsurvey.aws', 'flatsurvey.surfaces', 'flatsurvey.reporting', 'flatsurvey.worker', 'flatsurvey.cache', 'flatsurvey.ui'],
+    packages = ['flatsurvey', 'flatsurvey.cache', 'flatsurvey.jobs', 'flatsurvey.pipeline', 'flatsurvey.aws', 'flatsurvey.surfaces', 'flatsurvey.reporting', 'flatsurvey.worker', 'flatsurvey.cache', 'flatsurvey.ui', 'flatsurvey.test'],
     license = 'GPL 3.0+',
     long_description = open('README.md').read(),
     include_package_data=True,
